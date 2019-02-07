e64f8fac6694957fb7bdfe83ecc228a45faab68e
Shaun Lee <yngwieli10@gmail.com>
2019-02-07 16:08:10 +0800

    second commit

--- password-try.py
+++ password-try.py
@@ -1,12 +1,10 @@
-n=2
-while True:
-    psssword = input('請輸入密碼:')
-    if psssword == 'a123456':
-        print('密碼正確')
+password = 'a123456'
+n = 3
+while n>0:
+    psd = input('請輸入密碼:')
+    if psd == password:
+        print('登入成功')
         break
     else:
-        if n >0:
-            print(f'你還有{n}次機會')
-            n -=1
-        else:
-            break
+        n = n-1
+        print('密碼錯誤你還有', n,'次機會')
